e348ef18e13ade79f16f064b567b48ef120cedbb
CRULCOUNT <alflorence.abuan@student.dmmmsu.edu.ph>
2022-02-16 11:08:47 +0800

    Draft only

---
+++ vue/New Component/LoginSys.vue
@@ -0,0 +1,258 @@
+<template>
+    <div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <div class="max-w-md w-full space-y-8">
+        <div>
+          <img src="https://www.dmmmsu.edu.ph/wp-content/uploads/2019/06/DMMMSU-Logo-Final.png" alt="dmmmsu_logo" class="mx-auto h-40 w-auto">
+          <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">IMLUC VOTANTE</h2>
+          <p class="mt-2 text-center text-sm text-gray-600">
+            {{ ' ' }}
+
+          </p>
+        </div>
+        <form class="mt-8 space-y-6" action="#" method="POST">
+          <input type="hidden" name="remember" value="true" />
+          <div class="rounded-md shadow-sm -space-y-px">
+            <div>
+              <label for="email-address" class="sr-only">Student ID</label>
+              <input id="email-address" name="email" type="email" autocomplete="email" required="" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="ID Number" />
+            </div>
+            <div>
+              <label for="password" class="sr-only">Password</label>
+              <input id="password" name="password" type="password" autocomplete="current-password" required="" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password" />
+            </div>
+          </div>
+
+          <div class="flex items-center justify-between">
+            <div class="flex items-center">
+              <svg class="w-2 h-2 absolute top-0 right-0 m-4 pointer-events-none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 412 232"><path d="M206 171.144L42.678 7.822c-9.763-9.763-25.592-9.763-35.355 0-9.763 9.764-9.763 25.592 0 35.355l181 181c4.88 4.882 11.279 7.323 17.677 7.323s12.796-2.441 17.678-7.322l181-181c9.763-9.764 9.763-25.592 0-35.355-9.763-9.763-25.592-9.763-35.355 0L206 171.144z" fill="#648299" fill-rule="nonzero"/></svg>
+              <select class="rounded-full text-gray-600 h-10 pl-5 pr-10 bg-white hover:drop-shadow-lg focus:outline-none appearance-none ">
+                <option>Choose Account Type</option>
+                <option>Voter</option>
+                <option>Admin</option>
+              </select>
+            </div>
+
+          </div>
+
+          <div>
+            <button type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            <span class="absolute left-0 inset-y-0 flex items-center pl-3">
+              <LockClosedIcon class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
+            </span>
+              Sign in
+            </button>
+          </div>
+          <div class="">
+            <p>Checking Elections?
+              <a href="#!" class="font-medium text-black font-bold hover:text-blue-500">Click here</a> to go on Campaign Page
+            </p>
+          </div>
+        </form>
+      </div>
+    </div>
+  <div class="max-w-screen-xl px-4 mx-auto md:px-8">
+    <div class="mb-10 md:mb-16">
+      <h2
+        class="
+        mb-4
+        text-2xl
+        font-bold
+        text-center text-gray-800
+        lg:text-3xl
+        md:mb-6
+      "
+      >
+        Our Team Section
+      </h2>
+
+      <p class="max-w-screen-md mx-auto text-center text-gray-500 md:text-lg">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis
+        perspiciatis omnis aspernatur impedit vel, consectetur laudantium nulla et
+        aliqua
+      </p>
+    </div>
+
+    <div class="grid gap-4 md:grid-cols-3">
+      <div class="p-4 shadow">
+        <div class="h-48 mb-2 overflow-hidden rounded-lg shadow-lg md:h-80">
+          <img
+            src="https://cdn.pixabay.com/photo/2016/11/21/12/42/beard-1845166__340.jpg"
+            alt="Image"
+            class="object-cover object-center w-full h-full"
+          />
+        </div>
+
+        <div class="flex flex-col items-center justify-center">
+          <div class="font-bold text-indigo-500 md:text-lg">John Doe</div>
+          <p class="mb-3 text-sm text-gray-500 md:text-base md:mb-4">
+            Founder / CEO
+          </p>
+
+          <div class="flex">
+            <div class="flex gap-4">
+              <a href="#">
+                <svg
+                  class="w-6 h-6 text-blue-600 fill-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"
+                  />
+                </svg>
+              </a>
+              <a href="#">
+                <svg
+                  class="w-6 h-6 text-blue-300 fill-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z"
+                  />
+                </svg>
+              </a>
+              <a href="#">
+                <svg
+                  class="w-6 h-6 text-blue-500 fill-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 448 512"
+                >
+                  <path
+                    d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"
+                  ></path>
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="p-4 shadow">
+        <div class="h-48 mb-2 overflow-hidden rounded-lg shadow-lg md:h-80">
+          <img
+            src="https://cdn.pixabay.com/photo/2016/11/18/19/07/happy-1836445__340.jpg"
+            alt="Image"
+            class="object-cover object-center w-full h-full"
+          />
+        </div>
+
+        <div class="flex flex-col items-center justify-center">
+          <div class="font-bold text-indigo-500 md:text-lg">John Doe</div>
+          <p class="mb-3 text-sm text-gray-500 md:text-base md:mb-4">
+            Web Developer
+          </p>
+
+          <div class="flex">
+            <div class="flex gap-4">
+              <a href="#">
+                <svg
+                  class="w-6 h-6 text-blue-600 fill-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"
+                  />
+                </svg>
+              </a>
+              <a href="#">
+                <svg
+                  class="w-6 h-6 text-blue-300 fill-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z"
+                  />
+                </svg>
+              </a>
+              <a href="#">
+                <svg
+                  class="w-6 h-6 text-blue-500 fill-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 448 512"
+                >
+                  <path
+                    d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"
+                  ></path>
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="p-4 shadow">
+        <div class="h-48 mb-2 overflow-hidden rounded-lg shadow-lg md:h-80">
+          <img
+            src="https://cdn.pixabay.com/photo/2018/01/15/07/52/woman-3083401__340.jpg"
+            alt="Image"
+            class="object-cover object-center w-full h-full"
+          />
+        </div>
+
+        <div class="flex flex-col items-center justify-center">
+          <div class="font-bold text-indigo-500 md:text-lg">Scarlet</div>
+          <p class="mb-3 text-sm text-gray-500 md:text-base md:mb-4">
+            Devops Engineer
+          </p>
+
+          <div class="flex">
+            <div class="flex gap-4">
+              <a href="#">
+                <svg
+                  class="w-6 h-6 text-blue-600 fill-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"
+                  />
+                </svg>
+              </a>
+              <a href="#">
+                <svg
+                  class="w-6 h-6 text-blue-300 fill-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z"
+                  />
+                </svg>
+              </a>
+              <a href="#">
+                <svg
+                  class="w-6 h-6 text-blue-500 fill-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 448 512"
+                >
+                  <path
+                    d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"
+                  ></path>
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <br>
+  </template>
+
+  <script>
+    import { LockClosedIcon } from '@heroicons/vue/solid'
+    import {BellIcon} from "@heroicons/vue/outline";
+
+    export default {
+      components: {
+        LockClosedIcon,
+        BellIcon,
+      },
+      name: "Almanyak"
+    }
+  </script>
+
+<style scoped>
+
+</style>
