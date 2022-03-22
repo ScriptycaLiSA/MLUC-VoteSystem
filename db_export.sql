a8e2b28f047ad612b82614cb4dab0887f266eac3
Jake Montizon <75973651+ScriptycaLiSA@users.noreply.github.com>
2022-03-22 21:45:07 +0800

    env + gitignore

--- db_export.sql
+++ db_export.sql
@@ -12,6 +12,32 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+
+-- Dumping database structure for votesys-mluc
+CREATE DATABASE IF NOT EXISTS `votesys-mluc` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
+USE `votesys-mluc`;
+
+-- Dumping structure for table votesys-mluc.admin_acct_models
+CREATE TABLE IF NOT EXISTS `admin_acct_models` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `fname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `lname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `imageUrl` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `role` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `organization` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `canMngStd` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `canMngCand` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `canMngRtt` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `canVvto` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `canMngRtmt` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `canMngUsers` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
 -- Dumping data for table votesys-mluc.admin_acct_models: ~2 rows (approximately)
 DELETE FROM `admin_acct_models`;
 /*!40000 ALTER TABLE `admin_acct_models` DISABLE KEYS */;
@@ -20,10 +46,26 @@ INSERT INTO `admin_acct_models` (`id`, `fname`, `lname`, `imageUrl`, `email`, `p
 	(2, 'mark jansen', 'Ballesteros', 'https://tinyurl.com/macmakagago', 'markjansen@gmail.com', '$2y$10$AFdMFCdCRVk7Rnt07YU/bur.bHiL7yOrsmL6fdy12Hmv0HD0zS6xy', 'superadmin', 'SBO Chairman-CIT', 'true', 'true', 'true', 'true', 'true', 'true', '2022-03-18 09:53:19', '2022-03-18 09:53:19');
 /*!40000 ALTER TABLE `admin_acct_models` ENABLE KEYS */;
 
+-- Dumping structure for table votesys-mluc.candidate_models
+CREATE TABLE IF NOT EXISTS `candidate_models` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
 -- Dumping data for table votesys-mluc.candidate_models: ~0 rows (approximately)
 DELETE FROM `candidate_models`;
 /*!40000 ALTER TABLE `candidate_models` DISABLE KEYS */;
 /*!40000 ALTER TABLE `candidate_models` ENABLE KEYS */;
+
+-- Dumping structure for table votesys-mluc.colleges
+CREATE TABLE IF NOT EXISTS `colleges` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `initials` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Dumping data for table votesys-mluc.colleges: ~9 rows (approximately)
 DELETE FROM `colleges`;
@@ -40,10 +82,31 @@ INSERT INTO `colleges` (`id`, `name`, `initials`) VALUES
 	(9, 'College of Law', 'CLAW');
 /*!40000 ALTER TABLE `colleges` ENABLE KEYS */;
 
+-- Dumping structure for table votesys-mluc.failed_jobs
+CREATE TABLE IF NOT EXISTS `failed_jobs` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `uuid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
 -- Dumping data for table votesys-mluc.failed_jobs: ~0 rows (approximately)
 DELETE FROM `failed_jobs`;
 /*!40000 ALTER TABLE `failed_jobs` DISABLE KEYS */;
 /*!40000 ALTER TABLE `failed_jobs` ENABLE KEYS */;
+
+-- Dumping structure for table votesys-mluc.migrations
+CREATE TABLE IF NOT EXISTS `migrations` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Dumping data for table votesys-mluc.migrations: ~9 rows (approximately)
 DELETE FROM `migrations`;
@@ -60,12 +123,36 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 	(27, '2022_03_17_024131_create_voter_acct_models_table', 1);
 /*!40000 ALTER TABLE `migrations` ENABLE KEYS */;
 
+-- Dumping structure for table votesys-mluc.password_resets
+CREATE TABLE IF NOT EXISTS `password_resets` (
+  `username` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  KEY `password_resets_username_index` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
 -- Dumping data for table votesys-mluc.password_resets: ~0 rows (approximately)
 DELETE FROM `password_resets`;
 /*!40000 ALTER TABLE `password_resets` DISABLE KEYS */;
 /*!40000 ALTER TABLE `password_resets` ENABLE KEYS */;
 
--- Dumping data for table votesys-mluc.personal_access_tokens: ~10 rows (approximately)
+-- Dumping structure for table votesys-mluc.personal_access_tokens
+CREATE TABLE IF NOT EXISTS `personal_access_tokens` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `tokenable_type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tokenable_id` bigint(20) unsigned NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `abilities` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+  KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=49 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Dumping data for table votesys-mluc.personal_access_tokens: ~11 rows (approximately)
 DELETE FROM `personal_access_tokens`;
 /*!40000 ALTER TABLE `personal_access_tokens` DISABLE KEYS */;
 INSERT INTO `personal_access_tokens` (`id`, `tokenable_type`, `tokenable_id`, `name`, `token`, `abilities`, `last_used_at`, `created_at`, `updated_at`) VALUES
@@ -82,17 +169,59 @@ INSERT INTO `personal_access_tokens` (`id`, `tokenable_type`, `tokenable_id`, `n
 	(48, 'App\\Models\\AdminAcctModel', 1, 'main', 'fe44dba59dc242b473604fa9487704b978f1248f84a8953ed0e32d8d80f3e3e0', '["*"]', NULL, '2022-03-22 12:27:11', '2022-03-22 12:27:11');
 /*!40000 ALTER TABLE `personal_access_tokens` ENABLE KEYS */;
 
+-- Dumping structure for table votesys-mluc.sessions
+CREATE TABLE IF NOT EXISTS `sessions` (
+  `id` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_id` bigint(20) unsigned DEFAULT NULL,
+  `ip_address` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `user_agent` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `payload` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `last_activity` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `sessions_user_id_index` (`user_id`),
+  KEY `sessions_last_activity_index` (`last_activity`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
 -- Dumping data for table votesys-mluc.sessions: ~0 rows (approximately)
 DELETE FROM `sessions`;
 /*!40000 ALTER TABLE `sessions` DISABLE KEYS */;
 /*!40000 ALTER TABLE `sessions` ENABLE KEYS */;
+
+-- Dumping structure for table votesys-mluc.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `studentId` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `adminUname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_email_unique` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Dumping data for table votesys-mluc.users: ~0 rows (approximately)
 DELETE FROM `users`;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 
--- Dumping data for table votesys-mluc.voter_acct_models: ~2 rows (approximately)
+-- Dumping structure for table votesys-mluc.voter_acct_models
+CREATE TABLE IF NOT EXISTS `voter_acct_models` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `fname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `lname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `college` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Dumping data for table votesys-mluc.voter_acct_models: ~3 rows (approximately)
 DELETE FROM `voter_acct_models`;
 /*!40000 ALTER TABLE `voter_acct_models` DISABLE KEYS */;
 INSERT INTO `voter_acct_models` (`id`, `fname`, `lname`, `email`, `password`, `college`, `created_at`, `updated_at`) VALUES
@@ -100,6 +229,15 @@ INSERT INTO `voter_acct_models` (`id`, `fname`, `lname`, `email`, `password`, `c
 	(2, 'Mark Jansen', 'Ballesteros', 'mark.jansen@student.dmmmsu.edu.ph', '$2y$10$iM2Uwsro7jeT6PFD4nYVrOo.C8bWvGrT2222XU/aiNDV8i2.DgAh2', 'College of Information Technology', '2022-03-22 07:44:54', '2022-03-22 07:44:54'),
 	(3, 'Alflorence', 'Abuan', 'alflorence.abuan@student.dmmmsu.edu.ph', '$2y$10$6DcX4pkQ7Nts/6MWDtDBWeiulsZB/kS/1o4t4JhZ7zBdROamDRxgG', 'College of Information Technology', '2022-03-22 11:21:58', '2022-03-22 11:21:58');
 /*!40000 ALTER TABLE `voter_acct_models` ENABLE KEYS */;
+
+-- Dumping structure for table votesys-mluc.voter_models
+CREATE TABLE IF NOT EXISTS `voter_models` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `idNum` bigint(20) NOT NULL,
+  `fname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `lname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Dumping data for table votesys-mluc.voter_models: ~3 rows (approximately)
 DELETE FROM `voter_models`;
