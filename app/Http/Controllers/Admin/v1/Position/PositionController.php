eb8931ce682d55732c6df9ffca58ba0867316af4
Jake Montizon <75973651+ScriptycaLiSA@users.noreply.github.com>
2022-04-28 14:02:14 +0800

    Bug fix in hot reload + UI changes in dashboard 28042022

--- app/Http/Controllers/Admin/v1/Position/PositionController.php
+++ app/Http/Controllers/Admin/v1/Position/PositionController.php
@@ -4,25 +4,11 @@ namespace App\Http\Controllers\Admin\v1\Position;
 
 use App\Http\Controllers\Controller;
 use App\Models\Admin\PositionModel;
-use http\Env\Request;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class PositionController extends Controller
 {
-    public function index()
-    {
-        $data = PositionModel::all();
-
-        if (!$data == null) {
-            return response([
-                'success' => $data
-            ], 200);
-        }
-        return response([
-            'error' => 'Something went wrong. Please try again later!'
-        ]);
-    }
-
     public function createPosition(Request $request){
         $data = $request->only('name');
 
@@ -40,5 +26,18 @@ class PositionController extends Controller
         return response([
             'error'=>'Something went wrong. Please try again later!'
         ],500);
+    }
+    public function index()
+    {
+        $data = PositionModel::all();
+
+        if (!$data == null) {
+            return response([
+                'success' => $data
+            ], 200);
+        }
+        return response([
+            'error' => 'Something went wrong. Please try again later!'
+        ]);
     }
 }
