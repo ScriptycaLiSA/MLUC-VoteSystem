6b5bed8dd36fc700618d9fe35f3109785d01f399
Jake Montizon <75973651+ScriptycaLiSA@users.noreply.github.com>
2022-05-05 22:20:41 +0800

    Campaing + SQL 05052022

--- app/Http/Controllers/Admin/v1/CampaignSiteController.php
+++ app/Http/Controllers/Admin/v1/CampaignSiteController.php
@@ -20,7 +20,7 @@ class CampaignSiteController extends Controller
             ->join('election_models', 'candidate_models.election_id', '=', 'election_models.id')
             ->join('partylist_models', 'candidate_models.partylist_id', '=', 'partylist_models.id')
             ->join('colleges_models', 'candidate_models.college_init', '=', 'colleges_models.initials')
-            ->select('candidate_models.*', 'position_models.pos_name', 'partylist_models.party_name', 'colleges_models.coll_name')
+            ->select('candidate_models.*', 'position_models.pos_name', 'partylist_models.party_name', 'colleges_models.coll_name','election_models.elec_name')
             ->orderBy('candidate_models.position_id')
             ->get();
 
