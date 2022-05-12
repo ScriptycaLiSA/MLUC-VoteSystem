cd3c9c7c0db9382568609f66d0444add926a1f4f
CRULCOUNT <alflorence.abuan@student.dmmmsu.edu.ph>
2022-05-12 23:52:34 +0800

    Index Changes 120522

--- vue/src/views/voter/voterUtility/VoterIndex.vue
+++ vue/src/views/voter/voterUtility/VoterIndex.vue
@@ -2,89 +2,179 @@
   <div id="axiosForm">
     <div class="loader" v-if="loading"></div>
     <div class="bg-slate-100 shadow-xl min-h-screen">
+      <!--
+            <div class="" v-if="ballotData.length > 1">
+              <div class="text-2xl sm:text-md mb-2 text-left py-4 uppercase">election ballot</div>
+              <form @submit.prevent="castVote">
+                <div id="table1" class="relative overflow-x-auto shadow-md sm:rounded-lg" v-for="(i, k) in ballotPositions" :key="k">
+                  <div class="">
+                    <div class="py-4 ">
+                      <div class="">
+                        <div class=" ">
+                          {{ i.pos_name }}
+                        </div>
+                        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-700">
+                          <thead class="text-center text-sm text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                          <tr>
+                            <th scope="col"
+                                class="text-center py-4 px-2 text-sm text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                              Select
+                            </th>
+                            <th scope="col"
+                                class="text-center px-2 py-2 text-sm text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                              Profile
+                            </th>
+                            <th scope="col"
+                                class="">
+                              Name
+                            </th>
+                            <th scope="col"
+                                class="">
+                              Partylist
+                            </th>
+                          </tr>
+                          </thead>
 
-      <div class="text-4xl sm:text-sm md:text-sm px-16 " v-if="ballotData.length > 1">
-        <div class="border border-slate-100 text-4xl sm:text-md px-16 uppercase">election ballot</div>
-        <form @submit.prevent="castVote">
-          <div id="table1" class="flex flex-col mx-4" v-for="(i, k) in ballotPositions" :key="k">
-            <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
-              <div class="inline-block py-4 min-w-full sm:px-6 lg:px-8">
-                <div class="overflow-hidden shadow-md sm:rounded-lg">
-                  <div class="bg-grey-50 dark:bg-gray-400 px-2 py-3 border-solid border-grey-light border-b">
-                    {{ i.pos_name }}
+                          <tbody v-for="(index, key) in ballotData" :key="key">
+                          <tr class="dark:bg-gray-700 dark:text-gray-400" v-if="index.position_id==i.id">
+                            <td class="px-3 py-3">
+                              <div class="form-check">
+                                <input
+                                  v-model="selected[k]"
+                                  class="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                                  type="radio" :value="index.id" :id="i.id" :name="i.pos_name"
+                                  :disabled="success">
+                              </div>
+                            </td>
+                            <td class="px-2 py-2">
+                              <div class="flex w-12 h-12">
+                                <img class="rounded-full border border-gray-100 shadow-sm"
+                                     :src="getImgInfo(index.image)" alt="user image"/>
+                              </div>
+                            </td>
+                            <td class="py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">
+                              <label class="form-check-label text-white mb-12">
+                                {{ index.cand_name }}
+                              </label>
+                            </td>
+                            <td class="py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">
+                              <label class="form-check-label text-black mb-12">
+                              {{ index.party_name }}
+                              </label>
+                            </td>
+                          </tr>
+                          </tbody>
+
+                        </table>
+                      </div>
+                    </div>
                   </div>
-                  <table class="min-w-full">
-                    <thead class="bg-gray-50 dark:bg-gray-700">
-                    <tr>
-                      <th scope="col"
-                          class="py-3 px-6 text-xs font-medium tracking-wider text-center text-gray-700 uppercase dark:text-gray-400">
-                        Select
-                      </th>
-                      <th scope="col"
-                          class="py-3 px-6 text-xs font-medium tracking-wider text-center text-gray-700 uppercase dark:text-gray-400">
-                        Profile
-                      </th>
-                      <th scope="col"
-                          class="py-3 px-6 text-xs font-medium tracking-wider text-center text-gray-700 uppercase dark:text-gray-400">
-                        Name
-                      </th>
-                      <th scope="col"
-                          class="py-3 px-6 text-xs font-medium tracking-wider text-center text-gray-700 uppercase dark:text-gray-400">
-                        Partylist
-                      </th>
-                    </tr>
-                    </thead>
+                </div>
+                <span>Selected candidates: {{selected}}</span>
 
-                    <tbody v-for="(index, key) in ballotData" :key="key">
-                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700" v-if="index.position_id==i.id">
-                      <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                        <div class="form-check">
-                          <input
-                            v-model="selected[k]"
-                            class="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
-                            type="radio" :value="index.id" :id="i.id" :name="i.pos_name"
-                            :disabled="success">
-                        </div>
-                      </td>
-                      <td class="py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">
-                        <div class="relative w-14 h-14">
-                          <img class="rounded-full border border-gray-100 shadow-sm"
-                               :src="getImgInfo(index.image)" alt="user image"/>
-                        </div>
-                      </td>
-                      <td class="py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">
-                        <label class="form-check-label text-white mb-12">
-                          {{ index.cand_name }}
-                        </label>
-                      </td>
-                      <td class="py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">
-                        {{ index.party_name }}
-                      </td>
-                      <td class="hidden py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">
-                      </td>
-                    </tr>
-                    </tbody>
+                <button
+                  type="submit"
+                  class="my-2 uppercase flex-auto mx-10 block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                >
+                  cast vote
+                </button>
+              </form>
+            </div>
+          -->
+      <!--BOBOBOMOMOO-->
 
-                  </table>
+      <div
+        class="bg-white-50 rounded-lg shadow-xl grid grid-cols-1 lg:grid-cols-1 xl:grid-cols-1 2xl:grid-cols-1 px-2 min-w-screen">
+        <div class="py-4 px-4 max-w-lg"></div>
+        <div>
+          <form @submit.prevent="castVote">
+            <!-- div wrap v-if -->
+            <div class="" v-if="ballotData.length > 1">
+              <div class="text-2xl sm:text-md mb-2 text-left py-4 uppercase">election ballot</div>
+              <label for="table1" class="font-semibold text-black md:hidden lg:hidden xl:hidden 2xl:hidden">Slide the
+                table
+                left to right</label>
+              <div id="table1" class="flex flex-col mx-4" v-for="(i, k) in ballotPositions" :key="k">
+                <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+                  <div class="inline-block py-4 min-w-full sm:px-6 lg:px-8">
+                    <div class="overflow-hidden shadow-md sm:rounded-lg">
+                      <div class="bg-grey-50 dark:bg-gray-400 px-2 py-3 border-solid border-grey-light border-b">
+                        {{ i.pos_name }}
+                      </div>
+                      <table class="min-w-full">
+                        <thead class="bg-gray-50 dark:bg-gray-700">
+                        <tr>
+                          <th scope="col"
+                              class="py-3 px-6 text-xs font-medium tracking-wider text-center text-gray-700 uppercase dark:text-gray-400">
+                            Select
+                          </th>
+                          <th scope="col"
+                              class="py-3 px-6 text-xs font-medium tracking-wider text-center text-gray-700 uppercase dark:text-gray-400">
+                            Profile
+                          </th>
+                          <th scope="col"
+                              class="py-3 px-6 text-xs font-medium tracking-wider text-center text-gray-700 uppercase dark:text-gray-400">
+                            Name
+                          </th>
+                          <th scope="col"
+                              class="py-3 px-6 text-xs font-medium tracking-wider text-center text-gray-700 uppercase dark:text-gray-400">
+                            Partylist
+                          </th>
+                        </tr>
+                        </thead>
+
+                        <tbody v-for="(index, key) in ballotData" :key="key">
+                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+                            v-if="index.position_id===i.id">
+                          <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            <div class="form-check">
+                              <input
+                                v-model="selected[k]"
+                                class="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                                type="radio" :value="index.id" :id="i.id" :name="i.pos_name"
+                                :disabled="success">
+                            </div>
+                          </td>
+                          <td class="py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">
+                            <div class="flex w-12 h-12">
+                              <img class="rounded-full border border-gray-100 shadow-sm"
+                                   :src="getImgInfo(index.image)" alt="user image"/>
+                            </div>
+                          </td>
+                          <td class="py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">
+                            <label class="form-check-label text-white mb-12">
+                              {{ index.cand_name }}
+                            </label>
+                          </td>
+                          <td class="py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">
+                            {{ index.party_name }}
+                          </td>
+                          <td class="hidden py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">
+                          </td>
+                        </tr>
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
                 </div>
               </div>
+              <span>Selected candidates: {{ selected }}</span>
+              <button
+                type="submit"
+                class="my-2 uppercase flex-auto mx-10 block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                cast vote
+              </button>
             </div>
-          </div>
-          <span>Selected candidates: {{selected}}</span>
-          <!--selected field here /casted vote-->
-          <button
-            type="submit"
-            class="my-2 uppercase flex-auto mx-10 block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            cast vote
-          </button>
-        </form>
-      </div>
 
-      <div v-else>
-        <div class="border border-slate-100 text-4xl sm:text-md md:text-8xl px-16">{{ voterMessage.message }}</div>
+            <div v-else>
+              <div class="border border-slate-100 text-4xl sm:text-md md:text-8xl px-16">
+                {{ voterMessage.message }}
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
-
     </div>
   </div>
 </template>
@@ -171,16 +261,16 @@ export default {
     }
   },
   methods: {
-    castVote(){
+    castVote() {
       this.loading = true
 
       store.dispatch('castVoteFromBallot', selected)
-        .then((response)=>{
+        .then((response) => {
           this.success = true
           alert(response.data.success)
 
         })
-        .catch((error)=>{
+        .catch((error) => {
 
         })
     }
