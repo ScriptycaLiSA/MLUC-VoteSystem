60b080e2212e0c00aca9e9d61a2749ade80a5c71
CRULCOUNT <alflorence.abuan@student.dmmmsu.edu.ph>
2022-05-14 09:52:05 +0800

    V_dash, Dash 140522

--- vue/src/components/voterLayout/VoterDashboardLayout.vue
+++ vue/src/components/voterLayout/VoterDashboardLayout.vue
@@ -13,12 +13,6 @@
         </div>
         <div class="hidden md:block">
           <div class="ml-4 flex items-center md:ml-6">
-            <button type="button"
-                    class="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-              <span class="sr-only">View notifications</span>
-              <BellIcon class="h-6 w-6" aria-hidden="true"/>
-            </button>
-
             <!-- Profile dropdown -->
             <Menu as="div" class="ml-3 relative">
               <div>
